08f0daaffc2bc87984fee61aa6f772a90549f5ec
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-06-20 14:55:23 +0300

    added sortBy

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,6 +6,8 @@ import {
   updateContact,
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
@@ -23,7 +25,10 @@ export const getContactByIdController = async (req, res, next) => {
 };
 
 export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+
+  const contacts = await getAllContacts({ page, perPage, sortBy, sortOrder });
 
   res.json({
     status: 200,
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,12 +1,43 @@
 import Joi from 'joi';
 
-export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().min(3).max(20),
-  isFavourite: Joi.boolean().default('false'),
-  contactType: Joi.string()
-    .valid('work', 'home', 'personal')
-    .default('personal')
-    .required(),
-});
+export const createContactSchema = Joi.object(
+  {
+    name: Joi.string().min(3).max(20).required().messages({
+      'string.base': 'Username should be a string',
+      'string.min': 'Username should have at least 3 characters',
+      'string.max': 'Username should have at most 20 characters',
+      'any.required': 'Username is required',
+    }),
+    phoneNumber: Joi.string().min(3).max(20).required(),
+    email: Joi.string().min(3).max(20),
+    isFavourite: Joi.boolean().default('false'),
+    contactType: Joi.string()
+      .valid('work', 'home', 'personal')
+      .default('personal')
+      .required(),
+  },
+  // {
+  //   timestamps: true,
+  //   versionKey: false,
+  // },
+);
+export const updateContactSchema = Joi.object(
+  {
+    name: Joi.string().min(3).max(20).messages({
+      'string.base': 'Username should be a string',
+      'string.min': 'Username should have at least 3 characters',
+      'string.max': 'Username should have at most 20 characters',
+      'any.required': 'Username is required',
+    }),
+    phoneNumber: Joi.string().min(3).max(20),
+    email: Joi.string().min(3).max(20),
+    isFavourite: Joi.boolean().default('false'),
+    contactType: Joi.string()
+      .valid('work', 'home', 'personal')
+      .default('personal'),
+  },
+  // {
+  //   timestamps: true,
+  //   versionKey: false,
+  // },
+);
